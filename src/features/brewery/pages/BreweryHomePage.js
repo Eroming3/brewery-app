24c8930f88b43328f92600b72283961301c1d92f
Erik Roming <eroming3@gmail.com>
2022-03-28 21:38:44 -0500

    Added Brewery home page, styling, and card information from API call

--- src/features/brewery/pages/BreweryHomePage.js
+++ src/features/brewery/pages/BreweryHomePage.js
@@ -1,10 +1,86 @@
 import React from 'react';
+import { BreweryService } from '../../../services';
+import { Row, Col, Button } from 'antd';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBeer} from "@fortawesome/free-solid-svg-icons";
 
 class BreweryHomePage extends React.Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    breweries: []
+  }
+}
+
+componentDidMount() {
+  this.getBreweries();
+}
+
+getBreweries = () => {
+    BreweryService.GetBreweries('breweries').then(data => {
+      this.setState({
+        breweries: data
+      })
+      console.log(data)
+    },
+    (error) => {
+      console.log(error(error.toString()))
+    }
+  );
+}
   render() {
     return (
       <div className="app-page-container mt-5">
         <h1>Erik's Brewery App</h1>
+          {
+            this.state.breweries.map((brewery) => (
+              <div className="brew-card card-align">
+                <Row gutter={[16, 16]} className="p-3 ml-4" style={{ width: '95%' }}>
+                    <Col span={24}>
+                        <FontAwesomeIcon
+                            className="beer float-right"
+                            icon={faBeer}
+                            size="3x"
+                        />
+                        <div
+                            className="card-title"
+                        >
+                            <div className="d-flex">
+                                <span>{brewery.name}</span>
+                            </div>
+                        </div>
+                        <div>
+                            {brewery.website_url ? <a href={brewery.website_url} target="_blank">{brewery.website_url}</a> : 'No URL'}
+                        </div>
+                    </Col>
+                    <Col span={24}>
+                        <hr />
+                        <div className="d-inline-flex">
+                            <div>
+                                <div className="sub-title">
+                                    Type
+                                </div>
+                                <p className="ml-0">
+                                    {brewery.brewery_type ? brewery.brewery_type : 'No Type'}
+                                </p>
+                            </div>
+                            <div className="ml-4">
+                              <span className="sub-title">
+                                  Address
+                              </span>
+                                <p>{`${brewery.street ? `${brewery.street}, ` : ''} ${brewery.city}, ${brewery.state}, ${brewery.postal_code}`}</p>
+                            </div>
+                        </div>
+                        <div className="float-right">
+                            <Button className="primary-btn">
+                                View Details
+                            </Button>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+            ))
+          }
       </div>
     )
   }
---
+++ src/services/index.js
@@ -0,0 +1,1 @@
+export * as BreweryService from './brewery/BreweryService';
